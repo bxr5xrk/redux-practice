81f667c0de06050fe94131346708e4c20c48c0db
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-06 14:41:12 +0300

    added working with arrays

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { addCustomers, removeCustomers } from "./store/customerReducer";
 
 function App() {
     const dispatch = useDispatch();
     const cash = useSelector((state) => state.cash.cash);
+    const customers = useSelector((state) => state.customers.customers);
 
     const addCash = (cash) => dispatch({ type: "ADD_CASH", payload: cash });
 
     const getCash = (cash) => dispatch({ type: "GET_CASH", payload: cash });
 
+    const addCustomer = (name) => {
+        const user = {
+            name,
+            id: Date.now(),
+        };
+        dispatch(addCustomers(user));
+    };
+
+    const removeCustomer = (id) => dispatch(removeCustomers(id));
+
     return (
         <div className="App">
             <h1>{cash}</h1>
-            <button onClick={() => addCash(Number(prompt("enter: ")))}>
-                Add
+            <div>
+                <button onClick={() => addCash(Number(prompt("enter: ")))}>
+                    Add
+                </button>
+
+                {cash > 0 && <button onClick={() => getCash(10)}>Get</button>}
+            </div>
+
+            <button onClick={() => addCustomer(prompt("enter: "))}>
+                Add Customer
             </button>
 
-            {cash > 0 && <button onClick={() => getCash(10)}>Get</button>}
+            {customers.length ? (
+                customers.map((obj, id) => (
+                    <div key={id} className="user">
+                        <h3>
+                            {id + 1}. {obj.name}
+                        </h3>
+                        <h4 onClick={() => removeCustomer(obj.id)}>remove</h4>
+                    </div>
+                ))
+            ) : (
+                <h1>користувачів немає</h1>
+            )}
         </div>
     );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -2,13 +2,27 @@ const initialState = {
     customers: [],
 };
 
+const ADD_CUSTOMERS = "ADD_CUSTOMERS";
+const REMOVE_CUSTOMERS = "REMOVE_CUSTOMERS";
+
 export const customerReducer = (state = initialState, action) => {
     switch (action.type) {
-        case "ADD_CASH":
-            return { ...state, cash: state.cash + action.payload };
-        case "GET_CASH":
-            return { ...state, cash: state.cash - action.payload };
+        case ADD_CUSTOMERS:
+            return {
+                ...state,
+                customers: [...state.customers, action.payload],
+            };
+        case REMOVE_CUSTOMERS:
+            return {
+                ...state,
+                customers: state.customers.filter(
+                    (obj) => obj.id !== action.payload
+                ),
+            };
         default:
             return state;
     }
 };
+
+export const addCustomers = (user) => ({ type: ADD_CUSTOMERS, payload: user });
+export const removeCustomers = (id) => ({ type: REMOVE_CUSTOMERS, payload: id })
